82df79bd77362635f8026876a3a1df8d9c3990e2
John Tucker <tuckrally@gmail.com>
2012-01-03 08:54:09 -0600

    Everything

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Topcounter::Application.config.secret_token = 'a0a45164de9c6d9ee1a76b1f51b4cc841c6025391d965a3e51e5dfeae1e0e8b17ce6d42480d9e6d76aee23ed62b7ff2322eec609a847160161e2b389fd141aa8'
