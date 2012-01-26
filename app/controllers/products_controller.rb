bc20c33a5bc6700398c1729c4cc4b717e3cb2884
Mike Cruz <michaeldcruz@gmail.com>
2012-01-26 16:20:42 -0600

    Now with paging

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.page(params[:page]).per(100)
   end
   
   def show
@@ -27,7 +27,7 @@ class ProductsController < ApplicationController
     #where p.line_items.count != (p.on_hand_qty - p.on_customer_order_qty)
     
     # ITERATE OVER LINE ITEMS PER PRODUCT IN SQL
-    @products_diff = Product.where("coalesce((select sum(1) from line_items where line_items.product_id = products.id), 0) != (products.on_hand_qty - products.on_customer_order_qty)")
+    @products_diff = Product.where("coalesce((select sum(1) from line_items where line_items.product_id = products.id), 0) != (products.on_hand_qty - products.on_customer_order_qty)").page(params[:page]).per(100)
 
     # OLD ITERATIVE APPROACH
     # Go through all products, for each product do this block - if you find a product where l i count not equal then put the product into the array products_diff
