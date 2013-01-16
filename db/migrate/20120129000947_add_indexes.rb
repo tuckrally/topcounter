6bd4822b4eaf81d2037072d97d8a7ae482d36025
Mike Cruz <michaeldcruz@gmail.com>
2013-01-16 11:06:19 -0600

    removing pages index

--- db/migrate/20120129000947_add_indexes.rb
+++
@@ -1,8 +0,0 @@
-class AddIndexes < ActiveRecord::Migration
-  def up
-    execute "ALTER TABLE `pages_linked_pages` ADD UNIQUE `page_id_linked_page_id` (`page_id`,`linked_page_id`)"
-  end
-
-  def down
-  end
-end
