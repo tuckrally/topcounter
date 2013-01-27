1a725afe82bd519b22c168e9625a6b4a64fb1ae1
Jean Bahnik <jeanbahnik@gmail.com>
2013-01-26 22:19:29 -0600

    new scoping for diff controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,4 @@
 class Product < ActiveRecord::Base
   has_many :line_items
   has_many :rack_counts, :through => :line_items
-
-  def self.non_zero
-    self.line_items.count - self.on_hand_qty != 0
-  end
 end
