f3be108b91a3b081076d66a1887f7d986fdf27dd
Jean Bahnik <jeanbahnik@gmail.com>
2013-01-26 22:05:54 -0600

    changed diff page scope

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,12 +2,12 @@ class ProductsController < ApplicationController
   def index
     @products = Product.page(params[:page]).per(100)
   end
-  
+
   def show
     @product = Product.find params[:id]
     @product_locations = Product.where(:id => params[:id]).joins(:rack_counts).group("rack_counts.id, rack_counts.rack, rack_counts.shelf").select("rack_counts.id as rack_count_id, rack_counts.rack, rack_counts.shelf, sum(1) as item_count")
   end
-  
+
   def new
     @product = Product.new
   end
@@ -23,13 +23,13 @@ class ProductsController < ApplicationController
   end
 
   def diff
-    
+
     # Find all product where (sum of scan count) on each line_items != (on_hand_qty - on_customer_order_qty) for each product ID
     #where p.line_items.count != (p.on_hand_qty - p.on_customer_order_qty)
-    
+
     # ITERATE OVER LINE ITEMS PER PRODUCT IN SQL
-    @products_diff = Product.where("coalesce((select sum(1) from line_items where line_items.product_id = products.id), 0) != (products.on_hand_qty - products.on_customer_order_qty)").page(params[:page]).per(100)
-  
+    @products_diff = Product.non_zero.page(params[:page]).per(100)
+
     @current_page = params[:page] ? params[:page] : 1
     # OLD ITERATIVE APPROACH
     # Go through all products, for each product do this block - if you find a product where l i count not equal then put the product into the array products_diff
@@ -39,7 +39,7 @@ class ProductsController < ApplicationController
     #  else
     #  end
     #end
-    
+
   end
 
 end
